1982d3f82122470a05abbd45b54c50a92f7c9269
alberttNash <j-ose-33@hotmail.com>
2014-10-31 16:40:17 -0600

    Mensajes
    Modificaciones en Mensajes, 30-oct-2014

---
+++ red_itq(1).sql
@@ -0,0 +1,146 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-10-2014 a las 17:10:19
+-- Versión del servidor: 5.6.17
+-- Versión de PHP: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `red_itq`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE IF NOT EXISTS `categorias` (
+  `id_categorias` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id_categorias`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id_categorias`, `descripcion`) VALUES
+(1, 'nuevacategoria'),
+(2, 'Categoria2');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajes`
+--
+
+CREATE TABLE IF NOT EXISTS `mensajes` (
+  `id_mensaje` int(11) NOT NULL AUTO_INCREMENT,
+  `id_padre` int(11) NOT NULL DEFAULT '0',
+  `id_categoria` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `asunto` varchar(75) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish_ci NOT NULL,
+  `fecha_publicacion` date NOT NULL,
+  `archivo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id_mensaje`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=4 ;
+
+--
+-- Volcado de datos para la tabla `mensajes`
+--
+
+INSERT INTO `mensajes` (`id_mensaje`, `id_padre`, `id_categoria`, `id_usuario`, `asunto`, `descripcion`, `fecha_publicacion`, `archivo`) VALUES
+(1, 0, 1, 2, 'Todos van a reprobar D:', 'Si no hacemos nuestros manuales y nuestros algoritmos vamos a reprobar.', '2014-10-27', ''),
+(2, 1, 1, 1, 'Respuesta', 'Tengo que trabajar más que los demás', '2014-10-27', ''),
+(3, 1, 1, 3, 'Respuesta', 'Como soy de repe voy a pasar con 100', '2014-10-27', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `status`
+--
+
+CREATE TABLE IF NOT EXISTS `status` (
+  `id_status` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id_status`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=8 ;
+
+--
+-- Volcado de datos para la tabla `status`
+--
+
+INSERT INTO `status` (`id_status`, `descripcion`) VALUES
+(1, 'Activo'),
+(2, 'Suspendido'),
+(3, 'Eliminado'),
+(5, 'prueba1'),
+(6, 'prueba2'),
+(7, 'nuevo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `tipo_usuario` (
+  `id_tipo_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id_tipo_usuario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=32 ;
+
+--
+-- Volcado de datos para la tabla `tipo_usuario`
+--
+
+INSERT INTO `tipo_usuario` (`id_tipo_usuario`, `descripcion`) VALUES
+(1, 'prueba1'),
+(2, 'prueba2'),
+(6, 'prueba3'),
+(7, 'prueba4'),
+(31, 'Privilegiado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_corto` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre_largo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `contrasena` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `id_tipo_usuario` int(11) NOT NULL,
+  `id_status` int(11) NOT NULL,
+  `correo_electronico` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci AUTO_INCREMENT=5 ;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre_corto`, `nombre_largo`, `contrasena`, `id_tipo_usuario`, `id_status`, `correo_electronico`) VALUES
+(1, 'Alejandro', 'Villalvazo', 'hola', 1, 1, 'correo@correo.com'),
+(2, 'Selene', 'Grimaldo', 'otra', 2, 1, 'uncorreo@mail.com'),
+(3, 'Pancho', 'Perez', 'contra', 30, 2, 'uncorreo'),
+(4, 'Alex', 'Villalma', 'maalrasealex', 31, 1, 'mail@mail.com');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
